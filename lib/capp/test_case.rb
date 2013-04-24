81ade4d5d66cf9800e6573efb4e0771e620276a9
Eric Hodel <drbrain@segment7.net>
2013-04-23 23:09:39 -0700

    Document Capp::TestCase

--- lib/capp/test_case.rb
+++ lib/capp/test_case.rb
@@ -1,16 +1,57 @@
 require 'minitest/autorun'
 require 'capp'
 
+##
+# Capp::TestCase contains some useful methods for testing parts of Capp.
+#
+# The _DUMP constants are created from pcap files in the test directory.  You
+# can create your own capture from tcpdump:
+#
+#   tcpdump -r test/my.pcap [your specific capture arguments]
+
 class Capp::TestCase < MiniTest::Unit::TestCase
 
+  ##
+  # An ARP packet
+
   ARP_DUMP        = File.expand_path '../../../test/arp.pcap',    __FILE__
+
+  ##
+  # An EAP 802.1X packet
+
   EAP_802_1X_DUMP = File.expand_path '../../../test/802.1X.pcap', __FILE__
+
+  ##
+  # An ICMPv4 packet
+
   ICMP4_DUMP      = File.expand_path '../../../test/icmp4.pcap',  __FILE__
+
+  ##
+  # An ICMPv6 packet
+
   ICMP6_DUMP      = File.expand_path '../../../test/icmp6.pcap',  __FILE__
+
+  ##
+  # A TCPv4 packet
+
   TCP4_DUMP       = File.expand_path '../../../test/tcp4.pcap',   __FILE__
+
+  ##
+  # A TCPv6 packet
+
   TCP6_DUMP       = File.expand_path '../../../test/tcp6.pcap',   __FILE__
+
+  ## A UDPv4 packet
+
   UDP4_DUMP       = File.expand_path '../../../test/udp4.pcap',   __FILE__
+
+  ##
+  # A UDPv6 packet
+
   UDP6_DUMP       = File.expand_path '../../../test/udp6.pcap',   __FILE__
+
+  ##
+  # Returns the first packet in +dump+
 
   def packet dump
     Capp.offline(dump).loop.first
